b13d12edb0353bb42fcd5de4898af96ef91986a0
kaleb wondimu <kalebwondimu33@gmail.com>
2024-09-24 16:37:42 +0300

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./components/About";
+import Projects from "./components/Project";
+import Clients from "./sections/Clients";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+import Experiance from "./sections/Experience";
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <Experiance />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
