a344678d2c450bac6d0954e1bdb4c2e0f8d9f3a0
kaleb wondimu <kalebwondimu33@gmail.com>
2024-09-24 21:39:28 +0300

    some change done

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
-      <Clients />
+      {/* <Clients /> */}
       <Experiance />
       <Contact />
       <Footer />
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -75,7 +75,7 @@ const Contact = () => {
           className="absolute inset-0 min-h-screen"
         />
 
-        <div className="contact-container">
+        <div className="contact-container pt-6">
           <h3 className="head-text">Let's talk</h3>
           <p className="text-lg text-white-600 mt-3">
             Whether you’re looking to build a new website, improve your existing
